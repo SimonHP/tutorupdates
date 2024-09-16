a122f4def9a75c0b62151bc5c6487bd75a5d4ebf
SimonHP <simonhpalmer@icloud.com>
2024-09-16 10:09:46 +0100

    content

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
+#configure flak-mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Your SMTP server
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'simonhpalmer@gmail.com'
+app.config['MAIL_PASSWORD'] = 'hgjq xfse ssht palh'
+app.config['MAIL_DEFAULT_SENDER'] = 'simonhpalmer@gmail.com'
+
+mail = Mail(app)
 
 # Blog Page (Old index.html)
 @app.route('/')
@@ -14,6 +25,9 @@ def blog():
 def about():
     return render_template('about.html')
 
+
+
+
 # Contact Page
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
@@ -22,9 +36,11 @@ def contact():
         email = request.form['email']
         message = request.form['message']
         
-        # Here you could send the data to an email, save it in a database, etc.
-        print(f"Name: {name}, Email: {email}, Message: {message}")
-        
+        # compose email
+        msg = Message('New Contact Form Submission',
+                      recipients=['hastingssp1234@gmail.com','simon.palmer10@nhs.net'])
+        msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+        mail.send(msg)
         # Redirect to a thank-you page or back to the contact form
         return redirect(url_for('thank_you'))
 
